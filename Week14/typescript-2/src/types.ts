7a06b691d1dc74d079c5ab6f6241af5fef7746d8
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-11-14 06:06:42 +0530

    typescript - intersections in types

--- Week14/typescript-2/src/types.ts
+++ Week14/typescript-2/src/types.ts
@@ -11,3 +11,35 @@ function isItLeagal(user: Users) {
     return user.age >= 18;
 }
 console.log(isItLeagal(firstUser));
+
+// intersection (&):
+// The intersection type requires a variable to satisfy all specified types at once.It must include properties from both types, effectively merging them.
+type Employee = {
+    name: String;
+    startDate: String;
+}
+type Manager = {
+    name: String;
+    department: String;
+}
+
+// way to do intersection of types
+type TeamLead = Employee & Manager;
+
+let e: Employee = {
+    name: "Sada",
+    startDate: "01-02-2023"
+}
+let m: Manager = {
+    name: "Shiva",
+    department: "IT"
+}
+// merging employee and manager
+let t: TeamLead = {
+    name: "Sada", 
+    startDate: "01-02-2023",
+    department: "IT"
+}
+console.log(t.department);
+console.log(t.name);
+console.log(t.startDate);
