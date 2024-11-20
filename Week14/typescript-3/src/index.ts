fcf7a042ea59050d01f3bd80300775e5168ccdfd
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-11-20 06:41:42 +0530

    TS-3: Readonly api

--- Week14/typescript-3/src/index.ts
+++ Week14/typescript-3/src/index.ts
@@ -18,3 +18,38 @@ function updateUser(updatedProps: UpdateProps) {
 updateUser({
     name: "Rocky"
 })
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+/* we can change internal part of constant array or object but not the array/object itself.
+    eg: const a = [1, 2, 3];
+        a[0] = 5;               // it works(no error).
+        a = [2, 3, 4];          // it will not work.
+
+        What if we don't want to change the internal part of array/ object also?
+        for that we use "Readonly"
+*/
+// Readonly api
+
+type Users = {
+    name: String;
+    age: number;
+}
+const user: Readonly<Users> = {         // writing prefix as 'Readonly' makes them immutable.
+    name: "Sada",
+    age: 22
+}
+// user.age = 23;                       // gives error if I tried to change value of user's age/name.
+
+//  or assigining individually.
+/*
+type Users = {
+    readonly name: String;              // writing prefix as 'readonly' makes them immutable.
+    readonly age: number;
+}
+const user: Users = {
+    name: "Sada",
+    age: 22
+}
+user.name = "Shiva"
+*/
