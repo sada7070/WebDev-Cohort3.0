872215cf42ae74df211889da71f34ebc43725ec5
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-11-28 07:11:53 +0530

    Brainly - signup(BE)

--- Week15/Brainly/src/db.ts
+++ Week15/Brainly/src/db.ts
@@ -1,1 +1,8 @@
-// create user model and schema here
+import { model, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model("user", UserSchema);
--- Week15/Brainly/src/index.ts
+++ Week15/Brainly/src/index.ts
@@ -1,12 +1,53 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import z from "zod";
+import bcrypt from "bcrypt";
+import { UserModel } from "./db";
 
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    const{ username, password } = req.body;
+    
+    // input validation using zod
+    const requiredBody = z.object({
+        username: z.string().min(6).max(15),
+        password: z.string().min(8).max(20),
+    })
+    const  parseData = requiredBody.safeParse(req.body);
 
-})
+    if(!parseData.success) {
+        return res.status(422).json({
+            message: "Incorrect format.",
+            // to print the error
+            error: parseData.error
+        })
+    }
+
+    let errorThrown = false;
+
+    try{
+        //password hasing
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await UserModel.create({
+            username,
+            password: hashedPassword
+        })
+    } catch(e) {
+        res.status(409).json({
+            messsage: "Username already exist."
+        })
+        errorThrown = true;
+    }
+
+    if(!errorThrown) {
+        res.status(200).json({
+            message: "You are signed in."
+        })
+    }
+});
 
 app.post("/api/v1/signin", (req, res) => {
     
