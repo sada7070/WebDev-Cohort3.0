35c4db5fc808396bf92406c136fe8198549b95fa
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-12-21 01:09:28 +0530

    Prisma: inserting values to the table using ORM

--- Week18/prisma/src/index.ts
+++ Week18/prisma/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hi there");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+// the following code will insert value into the user table(no need to write raw query).
+async function createUSer() {
+    await client.user.create({
+        data: {
+            username: "Sada",
+            password: "123",
+            age: 22,
+            city: "Gpet"
+        }
+    })
+}
+
+createUSer();
