19f2e4fe1457acace2fde737f4875b495317b2bf
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-12-21 02:43:00 +0530

    Prisma: expressify it

--- Week18/prisma/src/index.ts
+++ Week18/prisma/src/index.ts
@@ -1,6 +1,39 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
 
+const app = express();
 const client = new PrismaClient();
+
+// expressifying prisma
+// to get all users info
+app.get("/users", async(req, res) => {
+    const users = await client.user.findMany();
+    res.json({
+        users
+    })
+})
+
+// to get specific user info including their todos
+app.get("/todos/:id", async(req, res) => {
+    const id = req.params.id;
+
+    const user = await client.user.findFirst({
+        where: {
+            id: parseInt(id)
+        },
+        select: {
+            todos: true,                            // getting todo info and user's name and id.
+            id: true,
+            username: true
+            
+        }
+    })
+    res.json({
+        user
+    })
+})
+
+app.listen(3000);
 
 // the following code will insert value into the user table(no need to write raw query).
 async function createUSer() {
