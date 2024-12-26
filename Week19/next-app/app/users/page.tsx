e1d2b95fb4023996dc12197bd00e1a74e83e08d7
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2024-12-27 02:19:05 +0530

    Nextjs: data fetching

---
+++ Week19/next-app/app/users/page.tsx
@@ -0,0 +1,17 @@
+// data fetching in Nextjs(using SSR(Server Side Rendering))
+import axios from "axios"
+
+// useEffect talks with client(browser) without the server, so we will unable to get nextjs benifits.
+// but this method directly talks with server not with client(browser)
+export default async function Users() {
+    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+    const data = response.data;
+
+    await new Promise(r => setTimeout(r, 5000));
+    return <div>
+        user page
+        <br></br>
+        {data.name}
+        {data.email}
+    </div>
+}
